5a15fa781437d4274f96abc5332d512c9cad95ad
lewislu327 <agooday32@gmail.com>
2021-08-30 19:37:05 +0800

    feat: Add admin restaurant post feature

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,6 +1,34 @@
+const { Mountain } = require('../models')
+
 const adminController = {
-  getMountains: (req, res) => {
-    return res.render('admin/mountains')
+  getMountains: async (req, res) => {
+    try {
+      const mountains = await Mountain.findAll({ raw: true })
+      return res.render('admin/mountains', { mountains })
+    } catch (error) {
+      console.error(error)
+    }
+  },
+
+  createMountain: (req, res) => {
+    return res.render('admin/create')
+  },
+
+  postMountain: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', "name didn't exist")
+      return res.redirect('back')
+    }
+    return Mountain.create({
+      name: req.body.name,
+      difficulty: req.body.difficulty,
+      address: req.body.address,
+      height: req.body.height,
+      description: req.body.description,
+    }).then((restaurant) => {
+      req.flash('success_messages', 'mountain was successfully created')
+      res.redirect('/admin')
+    })
   },
 }
 
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,5 +3,7 @@ const router = express.Router()
 const adminController = require('../../controllers/adminController')
 
 router.get('/', adminController.getMountains)
+router.get('/mountain/create', adminController.createMountain)
+router.post('/mountains', adminController.postMountain)
 
 module.exports = router
