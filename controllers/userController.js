aac2d907d04ea0cb1f4051807dd526afbea76e9f
lewislu327 <agooday32@gmail.com>
2021-08-30 14:11:46 +0800

    Confirm user's password and unique email

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,14 +6,31 @@ const userController = {
     return res.render('register')
   },
 
-  register: (req, res) => {
-    User.create({
-      name: req.body.name.trim(),
-      email: req.body.email.trim(),
-      password: bcrypt.hashSync(req.body.password.trim(), bcrypt.genSaltSync(10), null),
-    }).then((user) => {
-      return res.redirect('/signin')
-    })
+  register: async (req, res) => {
+    try {
+      if (req.body.passwordCheck !== req.body.password) {
+        req.flash('error_messages', '兩次密碼輸入不同！')
+        return res.redirect('/users/register')
+      }
+
+      const user = await User.findOne({ where: { email: req.body.email } })
+
+      if (user) {
+        req.flash('error_messages', '信箱重複！')
+        return res.redirect('/users/register')
+      }
+
+      await User.create({
+        name: req.body.name.trim(),
+        email: req.body.email.trim(),
+        password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null),
+      })
+
+      req.flash('success_messages', '成功註冊帳號！')
+      return res.redirect('users/signin')
+    } catch (error) {
+      console.error(error)
+    }
   },
 }
 
