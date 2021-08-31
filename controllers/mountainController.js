7989eea5f3e36aa387330c708d02183788ce6448
lewislu327 <agooday32@gmail.com>
2021-08-31 15:06:17 +0800

    feat: add pagination feature

--- controllers/mountainController.js
+++ controllers/mountainController.js
@@ -1,21 +1,52 @@
 const { Mountain, Altitude } = require('../models')
+const pageLimit = 10
 
 const mountainController = {
   getMountains: async (req, res) => {
+    let offset = 0
     const whereQuery = {}
     let altitudeId = ''
-    if (req.query.categoryId) {
-      altitudeId = Number(req.query.categoryId)
+    if (req.query.page) {
+      offset = (req.query.page - 1) * pageLimit
+    }
+    if (req.query.altitudeId) {
+      altitudeId = Number(req.query.altitudeId)
       whereQuery.AltitudeId = altitudeId
     }
-    let mountains = await Mountain.findAll({ where: whereQuery, include: Altitude })
-    const altitudes = await Altitude.findAll({ raw: true, nest: true })
+    const totalMountain = await Mountain.findAll({
+      raw: true,
+      nest: true,
+    })
+
+    let mountains = await Mountain.findAll({
+      where: whereQuery,
+      include: Altitude,
+      offset: offset,
+      limit: pageLimit,
+    })
+
     mountains = mountains.map((d) => ({
       ...d.dataValues,
       description: d.dataValues.description.substring(0, 50),
       altitudeName: d.Altitude.name,
     }))
-    return res.render('mountains', { mountains, altitudes, altitudeId })
+
+    const page = Number(req.query.page) || 1
+    const pages = Math.ceil(totalMountain.length / pageLimit)
+    const totalPage = Array.from({ length: pages }).map((item, index) => index + 1)
+    const prev = page - 1 < 1 ? 1 : page - 1
+    const next = page + 1 > pages ? pages : page + 1
+
+    const altitudes = await Altitude.findAll({ raw: true, nest: true })
+    return res.render('mountains', {
+      mountains,
+      altitudes,
+      altitudeId,
+      page: page,
+      totalPage: totalPage,
+      prev: prev,
+      next: next,
+    })
   },
 
   getMountain: async (req, res) => {
--- controllers/userController.js
+++ controllers/userController.js
@@ -38,7 +38,7 @@ const userController = {
 
   signIn: (req, res) => {
     req.flash('success_messages', '成功登入！')
-    res.redirect('/')
+    res.redirect('/mountains')
   },
 
   logout: (req, res) => {
