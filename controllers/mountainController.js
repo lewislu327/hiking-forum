a8373d4ea9bfe6c088734bc287d2d0db2bc8a7e3
lewislu327 <agooday32@gmail.com>
2021-08-31 13:41:56 +0800

    feat: Add user/mountains index page

--- controllers/mountainController.js
+++ controllers/mountainController.js
@@ -1,6 +1,14 @@
+const { Mountain, Altitude } = require('../models')
+
 const mountainController = {
-  getMountains: (req, res) => {
-    return res.render('mountains')
+  getMountains: async (req, res) => {
+    let mountains = await Mountain.findAll({ include: Altitude })
+    mountains = mountains.map((d) => ({
+      ...d.dataValues,
+      description: d.dataValues.description.substring(0, 50),
+      altitudeName: d.Altitude.name,
+    }))
+    return res.render('mountains', { mountains })
   },
 }
 
