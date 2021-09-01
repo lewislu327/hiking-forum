28b3a771d9f3597f5b5dd79af592da4bc942d1dd
lewislu327 <agooday32@gmail.com>
2021-09-01 13:45:06 +0800

    feat: add nation park open data & add user seed

--- seeders/20210831033453mountains-seed-file.js
+++ seeders/20210831033453mountains-seed-file.js
@@ -1,17 +1,17 @@
 'use strict'
 const faker = require('faker')
-
+const twNationalParksList = require('../public/tw-national-park.json')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert(
       'Mountains',
-      Array.from({ length: 50 }).map((d, i) => ({
-        name: faker.name.findName(),
+      Array.from({ length: 18 }).map((d, i) => ({
+        name: twNationalParksList[i].RA_name,
         height: Math.floor(Math.random() * 4000) + 'M',
-        address: faker.address.streetAddress(),
+        address: twNationalParksList[i].ADDRESS,
         difficulty: Math.floor(Math.random() * 5),
         image: `https://loremflickr.com/320/240/mountain/?random=${Math.random() * 100}`,
-        description: faker.lorem.text(),
+        description: twNationalParksList[i].RA_COPY,
         createdAt: new Date(),
         updatedAt: new Date(),
         AltitudeId: Math.floor(Math.random() * 5) * 10 + 1,
